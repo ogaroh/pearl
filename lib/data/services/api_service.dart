7912afda3626046a4e4c916b0189a6b03eb8b6b3
Erick Ogaro <ogaroerick1998@gmail.com>
2025-07-22 23:38:33 +0300

    feat: increase network delay in ApiService and update dependencies in pubspec

--- lib/data/services/api_service.dart
+++ lib/data/services/api_service.dart
@@ -4,12 +4,12 @@ import 'package:pearl/data/services/dummy_data.dart';
 class ApiService {
   // Mock API service methods
   Future<List<Item>> fetchItems() async {
-    await Future.delayed(Duration(seconds: 1)); // Simulate network delay
+    await Future.delayed(Duration(seconds: 2)); // Simulate network delay
     return (dummyJson).map((item) => Item.fromJson(item)).toList();
   }
 
   Future<Item> fetchItemById(String id) async {
-    await Future.delayed(Duration(seconds: 1)); // Simulate network delay
+    await Future.delayed(Duration(seconds: 2)); // Simulate network delay
     return (dummyJson)
         .map((item) => Item.fromJson(item))
         .firstWhere((item) => item.id == id);
