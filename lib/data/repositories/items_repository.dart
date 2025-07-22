9e9694d1a26c9cdfaee956c9d3d3568b71823e2b
Erick Ogaro <ogaroerick1998@gmail.com>
2025-07-23 00:29:37 +0300

    chore: remove placeholder delay in fetchItems method

--- lib/data/repositories/items_repository.dart
+++ lib/data/repositories/items_repository.dart
@@ -5,8 +5,6 @@ import '../models/item.dart';
 class ItemsRepository {
   final ApiService apiService = ApiService();
   Future<List<Item>> fetchItems() async {
-    // TODO: Replace with actual data source
-    await Future.delayed(Duration(seconds: 3));
     return apiService.fetchItems();
   }
 }
