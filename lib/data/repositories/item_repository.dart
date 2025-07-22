92f66607094e1f12d4d8b95a6617ad7f07b34cf9
Erick Ogaro <ogaroerick1998@gmail.com>
2025-07-22 23:42:47 +0300

    feat: create ItemRepository for managing item data access

---
+++ lib/data/repositories/item_repository.dart
@@ -0,0 +1,16 @@
+import 'package:pearl/data/models/item.dart';
+import 'package:pearl/data/services/api_service.dart';
+
+class ItemRepository {
+  final ApiService apiService = ApiService();
+
+  // This is where you would define your methods for accessing item data
+  Future<List<Item>> fetchItems() async {
+    return apiService.fetchItems();
+  }
+
+  // fetch items by id
+  Future<Item> fetchItemById(String id) async {
+    return apiService.fetchItemById(id);
+  }
+}
