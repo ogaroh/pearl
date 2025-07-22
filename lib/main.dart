c6b4195a2b61d2d3402b1f60d03ce1c3dfff6ed5
Erick Ogaro <ogaroerick1998@gmail.com>
2025-07-23 00:30:23 +0300

    fix: update home page title for consistency

--- lib/main.dart
+++ lib/main.dart
@@ -17,7 +17,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
-      home: const MyHomePage(title: 'Pearl Talent Assessment Home Page'),
+      home: const MyHomePage(title: 'Pearl Talent'),
     );
   }
 }
