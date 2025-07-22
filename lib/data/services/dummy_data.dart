06eb00eff7c20bdbb2a08b4a415616d5c87a72f8
Erick Ogaro <ogaroerick1998@gmail.com>
2025-07-22 23:31:55 +0300

    feat: add Item model and dummy JSON data for testing

---
+++ lib/data/services/dummy_data.dart
@@ -0,0 +1,28 @@
+// dummy json string for testing
+const String dummyJson = '''
+[
+  {
+    "id": "1",
+    "title": "Item 1",
+    "timestamp": "2024-10-01T12:00:00Z",
+    "tag": "New"
+  }, 
+  {
+    "id": "2",
+    "title": "Item 2",
+    "timestamp": "2024-10-02T12:00:00Z",
+    "tag": "Old"
+  },  
+  {
+    "id": "3",
+    "title": "Item 3",
+    "timestamp": "2024-10-03T12:00:00Z",
+    "tag": "Hot"
+  },  {
+    "id": "4",
+    "title": "Item 4",
+    "timestamp": "2024-10-04T12:00:00Z",
+    "tag": "Hot"
+  }
+]
+''';
